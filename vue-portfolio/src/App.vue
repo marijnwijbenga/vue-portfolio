00dcde6c0c343bdc412931440d0354354e220832
marijnwijbenga <m.wijbenga@outlook.com>
2020-08-26 13:09:43 +0200

    setup project with vue cli and inserted bootstrap CDN and did some testing to see if it all worked

---
+++ vue-portfolio/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container">
+    <h1>bootstrap test</h1>
+    <button class="btn btn-primary">some button </button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
