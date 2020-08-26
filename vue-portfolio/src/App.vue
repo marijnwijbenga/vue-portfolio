f486628b3f91cd18b23ef3b4065c3a767c1d814c
marijnwijbenga <m.wijbenga@outlook.com>
2020-08-26 15:14:28 +0200

    added bootstrap

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="container">
     <h1>bootstrap test</h1>
     <button class="btn btn-primary">some button </button>
+    <h2 class="text-primary">test</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -18,5 +19,5 @@ export default {
 </script>
 
 <style>
-
+  @import './assets/styles/bootstrap/bootstrap.css';
 </style>
