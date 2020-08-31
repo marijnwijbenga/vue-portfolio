96ae61d0833b79d8fd979d35a88ed0d9c6af1bf6
marijnwijbenga <m.wijbenga@outlook.com>
2020-08-31 17:03:50 +0200

    setting up the basics, working on navigation component, added bootstrap sass, customized theme colors

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="container">
+    <Navigation></Navigation>
     <h1>bootstrap test</h1>
     <button class="btn btn-primary">some button </button>
     <h2 class="text-primary">test</h2>
@@ -8,16 +9,19 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
+import Navigation from './components/Navigation'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navigation
   }
 }
 </script>
 
-<style>
-  @import './assets/styles/bootstrap/bootstrap.css';
+<style lang="scss">
+  @import './assets/styles/custom.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
---
+++ vue-portfolio/src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="my-5">
+        <nav class="navbar navbar-expand-lg navbar-info bg-info rounded navbar-dark">
+            <a class="font-weight-bold text-white" href="#">
+                <span class="text-secondary">
+                    dev.</span>portfolio
+                </a>
+            <button class="navbar-toggler" type="button"  @click="toggleMobileMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto mr-5 row">
+                    <li class="nav-item active col-sm">
+                        <a class="nav-link mr-4" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item col-sm">
+                        <a class="nav-link mr-4" href="#skills">Skills</a>
+                    </li>
+                    <li class="nav-item col-sm">
+                        <a class="nav-link mr-4" href="#projects">Projects</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>  
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navigation',
+        props: {
+
+        },
+        data() {
+
+            return {
+                
+            }
+        },
+        methods: {
+            toggleMobileMenu() {
+                console.log('this works');
+            }
+        }
+
+    }
+</script>
